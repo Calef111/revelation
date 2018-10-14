972e8e7714ee9c2513fbd42a34c3c75dd6500a7d
calef <calef@gmx.de>
2018-10-14 05:43:51 +0200

    better output

--- storage-check.lua
+++ storage-check.lua
@@ -5,5 +5,21 @@ stor = component.proxy(component.get("526"))
 energy = stor.getEnergyUsage()
 devs = stor.getStorages()
 
+totalCapItem = 0
+totalUseItem = 0
+totalCapLiq = 0
+totalUseLiq = 0
+for i = 1,#devs.devices,1 do
+    if (devs.devices[i].type == "item") then
+        totalCapItem += devs.devices[i].capacity
+        totalUseItem += devs.devices[i].usage
+    else
+        totalCapLiq += devs.devices[i].capacity
+        totalUseLiq += devs.devices[i].usage
+    end
+end
+
 print("Energy Usage:",energy)
-print("Devices:\n",devs)
+
+print("Items Stored:",totalUseItem,"/",totalCapItem,":",totalUseItem / totalCapItem * 100,"%")
+print("Items Stored:",totalUseLiq,"/",totalCapLiq,":",totalUseLiq / totalCapLiq * 100,"%")
